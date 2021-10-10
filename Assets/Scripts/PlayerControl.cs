1ec9bc9553da3e0d1cae11bbc0fe21e5b08b8fde
Dedy Setiadi <dedysetiadi1708@gmail.com>
2021-10-10 19:24:08 +0700

    updated masalah 4

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -19,15 +19,12 @@ public class PlayerControl : MonoBehaviour
     // Kecepatan gerak
     public float speed = 10.0f;
  
-    // Batas atas dan bawah game scene (Batas bawah menggunakan minus (-))
+    // Batas atas, bawah, kanan dan kiri game scene (Batas bawah menggunakan minus (-))
     public float yBoundary = 9.0f;
     public float xBoundary = 9.0f;
  
-    // Rigidbody 2D raket ini
+    // Rigidbody 2D bola ini
     private Rigidbody2D rigidBody2D;
- 
-    // Skor pemain
-    private int score;
 
     void Start()
     {
@@ -36,7 +33,7 @@ public class PlayerControl : MonoBehaviour
    
     void Update()
     {
-    // Dapatkan kecepatan raket sekarang.
+    // Dapatkan kecepatan bola sekarang.
         Vector2 velocity = rigidBody2D.velocity;
  
         // Jika pemain menekan tombol ke atas, beri kecepatan positif ke komponen y (ke atas).
@@ -78,16 +75,16 @@ public class PlayerControl : MonoBehaviour
         // Masukkan kembali kecepatannya ke rigidBody2D.
         rigidBody2D.velocity = velocity;
 
-        // Dapatkan posisi raket sekarang.
+        // Dapatkan posisi bola sekarang.
         Vector3 position = transform.position;
  
-        // Jika posisi raket melewati batas atas (yBoundary), kembalikan ke batas atas tersebut.
+        // Jika posisi bola melewati batas atas (yBoundary), kembalikan ke batas atas tersebut.
         if (position.y > yBoundary)
         {
             position.y = yBoundary;
         }
  
-        // Jika posisi raket melewati batas bawah (-yBoundary), kembalikan ke batas atas tersebut.
+        // Jika posisi bola melewati batas bawah (-yBoundary), kembalikan ke batas bawah tersebut.
         else if (position.y < -yBoundary)
         {
             position.y = -yBoundary;
@@ -100,13 +97,13 @@ public class PlayerControl : MonoBehaviour
         rigidBody2D.velocity = velocity;
 
  
-        // Jika posisi raket melewati batas atas (xBoundary), kembalikan ke batas atas tersebut.
+        // Jika posisi bola melewati batas kanan (xBoundary), kembalikan ke batas kanan tersebut.
         if (position.x > xBoundary)
         {
             position.x = xBoundary;
         }
  
-        // Jika posisi raket melewati batas bawah (-xBoundary), kembalikan ke batas atas tersebut.
+        // Jika posisi bola melewati batas kiri (-xBoundary), kembalikan ke batas kiri tersebut.
         else if (position.x < -xBoundary)
         {
             position.x = -xBoundary;
@@ -116,22 +113,4 @@ public class PlayerControl : MonoBehaviour
         transform.position = position;
     }
 
-
-    // Menaikkan skor sebanyak 1 poin
-    public void IncrementScore()
-    {
-        score++;
-    }
-    
-    // Mengembalikan skor menjadi 0
-    public void ResetScore()
-    {
-        score = 0;
-    }
- 
-    // Mendapatkan nilai skor
-    public int Score
-    {
-        get { return score; }
-    }
 }
